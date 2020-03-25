098ff798d263346efd6a20247416f2697c54f6e0
Pablo Baldini <pablo.baldini@gmail.com>
2020-03-25 22:19:38 +0000

    new message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kansas = Restaurant.new(name: 'Kansas', address: 'Buenos Aires', category: 'italian')
+wok_to_go = Restaurant.new(name: 'Wok to Go', address: 'London', category: 'chinese')
+epicure = Restaurant.new(name: 'Epicure', address: 'Paris', category: 'french')
+romario = Restaurant.new(name: 'Romario', address: 'Buenos Aires', category: 'italian')
+la_parolaccia = Restaurant.new(name: 'La parolaccia', address: 'Buenos Aires', category: 'italian')
+
+kansas.save
+wok_to_go.save
+epicure.save
+romario.save
+la_parolaccia.save
+
+
+
